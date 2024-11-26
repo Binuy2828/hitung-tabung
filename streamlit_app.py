a464ad9cd33c1943d22bcd1441fc287f9ed8a09d
Sabrina <bina04002@gmail.com>
2024-11-26 16:14:22 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,5 @@ import streamlit as st
 
 st.title("menghitung tabung :blue[volume tabung] :rocket:")
 
+r = st.number_input("Masukan jari-jari (cm):",0)
+t = st.number_input("Masukan tinggi (cm):",0)
